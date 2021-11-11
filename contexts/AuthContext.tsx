e9fbe3cc4cf03686a8e47edec149d966ecc35397
João Pedro <joaopsilvamagalhaes@gmail.com>
2021-11-10 21:15:03 -0300

    salvando dados do usuário

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,59 @@
+import { useRouter } from "next/dist/client/router";
+import { createContext, useState } from "react";
+import { api } from "../services/api";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  user: User;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const router = useRouter();
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", {
+        email,
+        password,
+      });
+
+      const { permissions, roles } = response.data;
+
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+
+      router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
